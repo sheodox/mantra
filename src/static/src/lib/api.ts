a4c206b254a25c0c6fe6fcd987396437804b2573
sheodox <3468630+sheodox@users.noreply.github.com>
2023-03-09 07:12:53 -0600

    Use correct paths
    The port for the api server was incorrect so http calls to the backend would fail, and the server executable isn't plural so it wasn't actually running the server.

--- src/static/src/lib/api.ts
+++ src/static/src/lib/api.ts
@@ -1,6 +1,6 @@
 import { dev } from '$app/environment';
 
-const API_HOST = !dev ? 'api:5004' : 'localhost:1323';
+const API_HOST = !dev ? 'api:1323' : 'localhost:1323';
 
 export const makeApiUrl = (pathname: string) => {
 	return `http://${API_HOST}${pathname}`;
