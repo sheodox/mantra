c66fb57ccc8eee41f68c90fe25ff84450021b406
sheodox <imagamer27@msn.com>
2023-03-23 15:38:19 -0500

    Revert back to a 4 hour minimum
    I thought the logarithm equation usage with the mantras would make it very occasionally send mantras more often, but in practice it's been sending them way too often still.

--- src/server/main.go
+++ src/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"math"
 	"math/rand"
 	"net/http"
 	"time"
@@ -13,10 +12,7 @@ import (
 )
 
 const (
-	// general starting point used for mantra timing calculations
-	BASE_MINIMUM_WAIT_TIME = 4 * time.Hour
-	// absolute shortest amount of time between mantras
-	MINIMUM_WAIT_TIME = 15 * time.Minute
+	MINIMUM_WAIT_TIME = 4 * time.Hour
 )
 
 var config Config
@@ -85,45 +81,17 @@ func scheduleNextMantra(m message.Messager) {
 	sendRandomMantra(m)
 
 	sleepUntilNextMantra()
-
 	scheduleNextMantra(m)
 }
 
 func sleepUntilNextMantra() {
-	// We should generally wait a while before the next mantra, but this lets mantras
-	// occasionally be sent more often.
-	minimum := BASE_MINIMUM_WAIT_TIME * time.Duration(getMinimumWaitTimeModifier())
-
 	// wait some randomish amount of time before sending the next mantra so it's never expected
 	// and hopefully isn't so often it loses all impact or gets ignored
 	extraTime := time.Duration(rand.Intn(8))*time.Hour + time.Duration(rand.Intn(60))*time.Minute
 
-	sleepTime := extraTime + minimum
-
-	// make sure we're not sending a new mantra too soon, it's very unlikely
-	// but still possible that we could spam a bunch of mantras if the minimum modifier
-	// and extra time were both low several times in a row.
-	if sleepTime < MINIMUM_WAIT_TIME {
-		sleepTime = MINIMUM_WAIT_TIME
-	}
+	sleepTime := extraTime + MINIMUM_WAIT_TIME
 
 	time.Sleep(sleepTime)
-}
-
-func getMinimumWaitTimeModifier() float64 {
-	x := rand.Float64()
-	// this is equivalent to doing a base 500 log, which gets a much steeper curve.
-	// so 90% of the time we're waiting 2/3 of the minimum at least, but for small
-	// x values it can dip all the way down towards 0 (under 0.002 it is negative,
-	// but it won't wait a time below MINIMUM_WAIT_TIME)
-	// x = 0.003 return 0.1
-	// x = 0.025 return 0.4
-	// x = 0.05 return 0.52
-	// x = 0.1 return 0.63
-	// x = 0.2 return 0.74
-	// x = 0.5 return 0.88
-	// x = 1.0 return 1
-	return math.Log(x)/math.Log(500) + 1
 }
 
 func sendRandomMantra(m message.Messager) {
